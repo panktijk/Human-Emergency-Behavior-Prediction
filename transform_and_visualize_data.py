1f0d8beffd14ba2f60d61024ce8f1fc74be374c4
Pankti Kansara <Pankti Kansara>
2017-03-28 05:14:27 -0700

    Sample paths

--- transform_and_visualize_data.py
+++ transform_and_visualize_data.py
@@ -3,27 +3,54 @@ import pandas as pd
 import time
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
+from datetime import datetime
 
 def epoch_to_datetime(epoch):
     return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(epoch))
+    
+def read_and_transform_data():
+    col_names = ['Cab_ID', 'Latitude', 'Longitude', 'Whether_Occupied', 'Timestamp']
+    cabs_df = pd.read_csv('Datasets\\cabsdata.csv', names = col_names)
+    cabs_df['Timestamp'] = cabs_df['Timestamp'].apply(lambda epoch: epoch_to_datetime(epoch))
+    earthquakes_df = pd.read_csv('Datasets\\Earthquakes\\nc_earthquakes.csv')
+    earthquakes_df = earthquakes_df[(earthquakes_df['longitude'] <= -122) & (earthquakes_df['place'].str.contains('San Francisco')) & (earthquakes_df['mag'] >= 2)]
+    return cabs_df, earthquakes_df
 
-col_names = ['Cab_ID', 'Latitude', 'Longitude', 'Whether_Occupied', 'Timestamp']
-df = pd.read_csv('Datasets\\cabsdata.csv', names = col_names)
-df['Timestamp'] = df['Timestamp'].apply(lambda epoch: epoch_to_datetime(epoch))
-df = df[df['Cab_ID'] <= 2]
+def trace_trajectory(df_temp, m):
+    lat = df_temp.Latitude.values
+    lon = df_temp.Longitude.values
+    x, y = m(lon, lat)
+    m.plot(x, y, 'o', markersize=5)
+    #plt.plot(x, y, '-', color=np.random.rand(3,))
+    plt.plot(x, y, '-', color='yellow')
+    
+def get_continuous_paths(df):
+    splits = np.append(np.where(np.diff(df['Whether_Occupied']) != 0)[0], len(df)+1)+1
+    prev = 0
+    paths_df = []
+    for split in splits:
+        paths_df += [df[prev:split]]
+        prev = split
+    return paths_df
+    
+def visualize(df):
+    paths_df = get_continuous_paths(df)
+    m = Basemap(llcrnrlon=-122.56,llcrnrlat=37.4,urcrnrlon=-122,urcrnrlat=37.99, epsg=4269)
+    #df1 = df.iloc[7797212:7797220]
+    for path_df in paths_df[:10]:
+        #if (1 in path_df['Whether_Occupied']):
+            trace_trajectory(path_df, m)   
+    #df2 = df.iloc[356768:356781]
+    #trace_trajectory(df2, m)
+    m.arcgisimage(service='ESRI_Imagery_World_2D', xpixels = 2500, verbose= True)
+    plt.show()
+    
+def get_earthquake_dates_and_locs(df):
+    dates = [t.split('T')[0] + ' ' + t.split('T')[1].split(':')[0] for t in df['time']]
+    lats = df['latitude']
+    lons = df['longitude']
+    return dates, lats, lons
+    
+cabs_df, earthquakes_df = read_and_transform_data()
+visualize(cabs_df)
 
-m = Basemap(width=20000,height=20000,projection='lcc', resolution='f',lat_0=37.76,lon_0=-122.4)
-m.drawcoastlines(linewidth=0.5)
-m.drawcountries(linewidth=0.5)
-#m.fillcontinents(color = 'gray')
-colors = {1:'blue', 2:'green'}
-m.drawmapboundary(fill_color='aqua')
-m.fillcontinents(color='white',lake_color='aqua')
-df = df[df['Whether_Occupied'] == 1]
-for row in df.groupby('Cab_ID').head(50):
-    lat = df['Latitude'].values
-    lon = df['Longitude'].values
-    x,y = m(lon, lat)
-    m.plot(x, y, 'ro', markersize=5)
-    plt.plot(x, y, '-', color=df['Cab_ID']) 
-plt.show()
